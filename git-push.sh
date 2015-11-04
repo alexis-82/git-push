ba5eeddfed8be8e354702b799374bb162cb746f7
alexis-82 <giobbia82@gmail.com>
2015-11-04 15:17:48 +0100

    new update

---
+++ git-push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -n 'Inserire nome repository: '
+read repo
+echo -n 'Nome user: '
+read login
+cd "$repo"
+git remote rm origin
+git remote add origin git@github.com:"$login"/"$repo"
+git push -u origin master
