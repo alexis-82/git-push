b26d799bf9487f35dafb1ee6503bfb083f1f55b3
alexis-82 <giobbia82@gmail.com>
2015-11-11 16:25:16 +0100

    fix file

--- git-push.sh
+++ git-push.sh
@@ -4,6 +4,7 @@ read repo
 echo -n 'Nome user: '
 read login
 cd "$repo"
+git pull --rebase origin master
 git remote rm origin
 git remote add origin git@github.com:"$login"/"$repo"
 git push -u origin master
