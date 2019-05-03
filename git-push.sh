a9469b10587874b3f70f3979660566466fc6467f
linux <alessioabrugiati@gmail.com>
2019-05-04 01:54:04 +0200

    fix

--- git-push.sh
+++ git-push.sh
@@ -12,3 +12,12 @@ git pull --rebase origin master
 git remote rm origin
 git remote add origin git@github.com:"$login"/"$repo"
 git push -u origin master
+
+
+
+# Other Commands
+#git init
+#git add -A
+#git commit -m 'Fix bad repo'
+#git push
+#git push -f origin master
