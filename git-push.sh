ff06d3cddb834f0fed9b84f86974aca75439aa5d
linux <alessioabrugiati@gmail.com>
2019-08-11 19:42:43 +0200

    add

--- git-push.sh
+++ git-push.sh
@@ -4,14 +4,22 @@ read repo
 echo -n 'Nome user: '
 read login
 cd "$repo"
-git add * -f
+git add .
 echo -n 'Scrivere il commit: '
 read comment
-git commit -am "$comment"
-git pull --rebase origin master
-git remote rm origin
-git remote add origin git@github.com:"$login"/"$repo"
-git push -u origin master
+git commit -ma "$comment"
+git push origin master
+
+
+# FUNZIONA
+#git add * -f
+#echo -n 'Scrivere il commit: '
+#read comment
+#git commit -am "$comment"
+#git pull --rebase origin master
+#git remote rm origin
+#git remote add origin git@github.com:"$login"/"$repo"
+#git push -u origin master
 
 #Try
 # Setting - Developer settings - Personal access tokens
